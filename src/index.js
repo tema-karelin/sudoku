1dd218775d1f0e12ddf687fb25ad4b7fb39f82d7
Artem Karelin <temi41995@gmail.com>
2019-03-27 01:45:29 +0300

    feat: add new function

--- src/index.js
+++ src/index.js
@@ -11,30 +11,67 @@ module.exports = function solveSudoku(matrix) {
   //   [0, 0, 7, 0, 0, 0, 0, 5, 0],
   //   [1, 6, 5, 3, 9, 0, 4, 7, 0]
   // ];
-  let unknownX = [];
-  let unknownY = [];
-  let sud = []; // Создание соразмерного массива
+  // Создание клона исходной матрицы
+  // Создание клона исходной матрицы
+  let m1 =[]; // Клон, который останется неизменным
+  let sud = []; // Создание соразмерного массива для решения
+  let unsud = [];
+  m1.length = 9;
+  let unknown = []; // Массив с координатами пустых ячеек
   sud.length = 9;
+  unsud.length = 9;
   for (let c=0; c<9; c++) {
+    m1[c] = [];
+    m1[c].length = 9;
     sud[c] = [];
     sud[c].length = 9;
+    unsud[c] = [];
+    unsud[c].length = 9;
   };
-// Поиск пустых элементов судоку
   for (let i=0; i<9; i++){  
     for (let j=0; j<9; j++){
-      if (matrix[i][j] == 0) {
-        unknownX.push(j); // Запись координаты X
-        unknownY.push(i); // Запись координаты Y
-        sud[i][j] = [];   // Создание пустого массива в пустом элементе
-      } else {sud[i][j] = matrix[i][j]};
+      m1[i][j] = matrix[i][j];
     };
-  };  // На выходе 3 массива. В первом координаты X, во втором - Y. 3 - судоку с пустыми массивами вместо 0 
+  };
+
+  function zeroArray(matrix) {
+    for (let i=0; i<9; i++){  
+      for (let j=0; j<9; j++){
+          if (matrix[i][j] == 0) {
+              sud[i][j] = [];   // Создание пустого массива в пустом элементе
+              unsud[i][j] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+            } else {sud[i][j] = matrix[i][j]; unsud[i][j] = matrix[i][j];};
+      };
+    };
+    return sud;
+  };
+
+  function emptyList(matrix) {
+    let unknownX = [];
+    let unknownY = [];
+    // Поиск пустых элементов судоку
+    for (let i=0; i<9; i++){  
+      for (let j=0; j<9; j++){
+        if (matrix[i][j] == 0) {
+          unknownX.push(j); // Запись координаты X
+          unknownY.push(i); // Запись координаты Y
+          
+        };
+      };
+    };
+    unknown = [unknownX, unknownY];
+    return unknown;  // На выходе 1 массив с двумя элементами, тоже массивы. В первом координаты X пустых ячеек, во втором - Y 
+  };
+
+  
+  
 // Добавление элементов массива в ячейку с пустым массивом. Массив содержит числа, которые не могут быть в этой ячейке.
-  for (let k=0; k<unknownX.length; k++) {  
-    let x = unknownX[k];
-    let y = unknownY[k];
+function exceptionRecord(unknown, matrix) {
+  for (let k=0; k<unknown[0].length; k++) {  
+    let x = unknown[0][k];
+    let y = unknown[1][k];
     let p = [];
-    for (i=0; i<9; i++) {
+    for (i=0; i<9; i++) { 
       if (matrix[i][x] != 0) {
         let value = matrix[i][x];
         p.push(value);
@@ -151,7 +188,30 @@ module.exports = function solveSudoku(matrix) {
     };
     sud[y][x] = np; 
   };
-// Поиск ячеек с 8 элементами массива и запись девятого(недостающего числа туда) 
+  return sud;
+};
+
+function unExceptionRecord(sud) {
+    for (i=0; i<9; i++) {
+        for (j=0; j<9; j++) {
+            for (c=0; c<sud[i][j].length; c++) {
+                for (k=0; k<9; k++) {
+                    if (sud[i][j][c] == unsud[i][j][k]) {
+                        unsud[i][j].splice(k, 1);
+                    };
+                };
+            };
+        };
+    };
+    return unsud;
+};
+
+function F_loner(matrix) {
+  zeroArray(matrix);
+  emptyList(matrix);
+  exceptionRecord(unknown, matrix);
+  unExceptionRecord(sud);
+// Поиск ячеек с 8 элементами массива и запись девятого(недостающего числа) вместо этого массива 
   for (i=0; i<9; i++) {
     for (j=0; j<9; j++) {
       p = sud[i][j];
@@ -161,9 +221,132 @@ module.exports = function solveSudoku(matrix) {
           composition = composition/(p[k]);
         };
         matrix[i][j] = composition;
+        F_loner(matrix);
       };
     };
   };
-  // console.log(unknownX, unknownY, sud, matrix);
+  return matrix;
+};
+function onlyVariantInRow(unsud) {
+    let rows = [];
+    let nrows = [];
+    let columns = [];
+    let ncolumns = [];
+    rows.length = 9;
+    nrows.length = 9;
+    columns.length = 9;
+    ncolumns.length = 9;
+    for (i=0; i<9; i++) {
+        rows[i] = [];
+        columns[i] = [];
+        nrows[i] = [];
+        ncolumns[i] = [];
+    };
+    // для строк (rows)
+    for (i=0; i<9; i++) {
+        for (j=0; j<9; j++) {
+            if (Array.isArray(unsud[i][j])) {
+                for (c=0; c<unsud[i][j].length; c++) {
+                    rows[i].push(unsud[i][j][c]);
+                    // columns[j].push(sud[i][j][c]);
+                };
+            };
+        };
+    };
+    for (i=0; i<9; i++) {
+        rows[i].sort();
+        // columns[i].sort();
+    };
+    for (i=0; i<9; i++) {
+        for (j=0; j<rows[i].length; j++) {
+            if (j == 0) {
+                if (rows[i][j] != rows[i][j+1]) {nrows[i].push(rows[i][j]);};
+            } else {
+                   if (j == rows[i].length - 1) {
+                       if (rows[i][j] != rows[i][j-1]) {nrows[i].push(rows[i][j]);};
+                   } else {
+                       if (rows[i][j] != rows[i][j-1] && rows[i][j] != rows[i][j+1]) {nrows[i].push(rows[i][j]);};
+                   };
+            };
+            
+        };
+    };
+    for (i=0; i<9; i++) {
+        if (nrows[i].length != 0) {
+            for (j=0; j<nrows[i].length; j++) {
+                for (c=0; c<9; c++) {
+                    if (Array.isArray(unsud[i][c])) {
+                        for (k=0; k<unsud[i][c].length; k++) {
+                            if (nrows[i][j] == unsud[i][c][k]) {
+                                matrix[i][c] = nrows[i][j];
+                            };
+                        };
+                    };
+                };
+            };
+        };
+    };
+    zeroArray(matrix);
+    emptyList(matrix);
+    exceptionRecord(unknown, matrix);
+    unExceptionRecord(sud);
+    //    для колонок (columns)
+    for (i=0; i<9; i++) {
+    for (j=0; j<9; j++) {
+        if (Array.isArray(unsud[i][j])) {
+            for (c=0; c<unsud[i][j].length; c++) {
+                // rows[i].push(sud[i][j][c]);
+                columns[j].push(unsud[i][j][c]);
+            };
+        };
+    };
+    };
+    for (i=0; i<9; i++) {
+    // rows[i].sort();
+    columns[i].sort();
+    };
+    for (i=0; i<9; i++) {
+    for (j=0; j<columns[i].length; j++) {
+        if (j == 0) {
+            if (columns[i][j] != columns[i][j+1]) {ncolumns[i].push(columns[i][j]);};
+        } else {
+               if (j == columns[i].length - 1) {
+                   if (columns[i][j] != columns[i][j-1]) {ncolumns[i].push(columns[i][j]);};
+               } else {
+                   if (columns[i][j] != columns[i][j-1] && columns[i][j] != columns[i][j+1]) {ncolumns[i].push(columns[i][j]);};
+               };
+        };
+        
+    };
+    };
+    for (i=0; i<9; i++) {
+    if (ncolumns[i].length != 0) {
+        for (j=0; j<ncolumns[i].length; j++) {
+            for (c=0; c<9; c++) {
+                if (Array.isArray(unsud[c][i])) {
+                    for (k=0; k<unsud[c][i].length; k++) {
+                        if (ncolumns[i][j] == unsud[c][i][k]) {
+                            matrix[c][i] = ncolumns[i][j];
+                            
+                        };
+                    };
+                };
+            };
+        };
+    };
+    };
+    F_loner(matrix);
+    return matrix;
+};
+
+  F_loner(matrix);
+  unExceptionRecord(sud);
+  onlyVariantInRow(unsud);
+  
+//   console.log(m1);
+//   console.log(matrix);
+//   console.log(sud);
+//   console.log(unknown);
+//   console.log(unsud);
   return matrix;
 }
